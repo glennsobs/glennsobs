f438cc38aa406e9febd81664255d0229a495d5ba
glennsobs <54005758+glennsobs@users.noreply.github.com>
2019-08-15 17:37:44 -0600

    rebuilding spider

---
+++ PageSpider/utilities/database_utilities.py
@@ -0,0 +1,8 @@
+def create_database(database_path: str):
+    #todo: generate database
+    pass
+
+
+def save_words_to_database(database_path:str, words_list: list):
+    #todo: save the words to the database
+    pass
---
+++ PageSpider/utilities/url_utilities.py
@@ -0,0 +1,13 @@
+def load_urls_from_file(file_path: str):
+    # TODO: Add the code needed to read the text file
+    pass
+
+
+def load_page(url: str):
+    # TODO: Add the code that reads the url
+    pass
+
+
+def scrape_page(page_contents: str):
+    # TODO: Analyze the text
+    pass
